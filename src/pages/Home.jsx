73fc59508b387a97db910f758fe99bc358bc4f1d
Rafael Reyes <rafaelreyes@Rafaels-Air.askey.com>
2023-03-15 23:08:14 -0400

    home page update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,61 +11,73 @@ const Home = () => {
           <img className="marchevent" src={marchevent1} alt="march event" />
         </Row>
         <Row className="patient-needs">
-          <Col xs={11}>
+          <Col xs={6}>
             <img
               className="patient-image-1"
               src="https://www.practicebuilders.com/blog/wp-content/themes/pbblog/postimg/2021/july/Do-You-Understand-Your-Patients-Needs-700X246.jpg"
               alt="patient-image1"
             />
-          </Col>
-          <Col xs={6}>
-            <ul>
-              <h4>
-                What Do We Offer for Our Patients?
-              </h4>
-                <ul>
-                  <li>
-                    Adult Social Care, Mental Health & Community Services: works
-                    collaboratively with adults and their families in South
-                    Florida alongside hospital colleagues and key Government
-                    departments.
-                  </li>
-                  <li>
-                    Primary and Preventative Care (PPC): is responsible for the
-                    undertaking of governance functions, performance management
-                    and oversight of the primary care service.
-                  </li>
-                  <li>
-                    Mental Health Services: Provide services through the clinic
-                    and referring partners to identify and treat mental health,
-                    neurological and substance abuse disorders.
-                  </li>
-                </ul>
-
-
-              <br /> Our own experience, based on years of caring for patients,
-              is that people seeking medical care have the following needs:
-              <br />● To be treated with respect and dignity <br />
-              <br />● To be actively engaged in a community of some kind <br />
-              <br />● To be involved in his/her own treatment and living plan{" "}
+            <h6 className="hOmeH6">What Do We Offer for Our Patients?</h6> <br />
+            <ul className="uList">
+              <li>
+                Adult Social Care, Mental Health & Community Services: works
+                collaboratively with adults and their families in South Florida
+                alongside hospital colleagues and key Government departments.
+              </li>
               <br />
-              <br />● To be cared for by skilled, medically-knowledgeable
-              significant need for quality home health care and social services
-              within this region and we believe that by employing competent and
-              well-educated staff and providing them with organized and
-              responsive management, we can become the best community health
-              care/social service organization of choice in Southern Florida.
+              <li>
+                Primary and Preventative Care (PPC): is responsible for the
+                undertaking of governance functions, performance management and
+                oversight of the primary care service.
+              </li>
+              <br />
+              <li>
+                Mental Health Services: Provide services through the clinic and
+                referring partners to identify and treat mental health,
+                neurological and substance abuse disorders.
+              </li>
+            </ul>
+            </Col>
+            <Col xs={5}>
+              <img
+                className="patient-exp-1"
+                src="https://www.usa.philips.com/c-dam/b2bhc/master/hts/PX-week/patientexperienceweekicon_transparant.png"
+                alt="patient experience"
+              />
+            
+            <ul className="uList">
+              <h6 className="hOmeH6">
+              Our own experience, based on years of caring for patients,
+              is that people seeking medical care have the following needs:
+              </h6> 
+              <br />
+              <li>
+                To be treated with respect and dignity 
+              </li>
+              <br />
+              <li>
+                To be actively engaged in a community of some kind
+           
+              </li>
+              <br />
+              <li>
+                To be involved in his/her own treatment and living plan
+              </li>
+              <br />
+              <li>
+                To be cared for by skilled, medically-knowledgeable
+                significant need for quality home health care and social
+                services within this region and we believe that by employing
+                competent and well-educated staff and providing them with
+                organized and responsive management, we can become the best
+                community health care/social service organization of choice in
+                Southern Florida.
+              </li>
             </ul>
           </Col>
-          <Col xs={6}></Col>
-          <br />
         </Row>
 
-        <Row>
-          <Col></Col>
-          <Col xs={3}></Col>
-          <Col></Col>
-        </Row>
+
       </Container>
     </>
   );
